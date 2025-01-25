2dca7253764a752a56aa54eb59d3e24544976c6a
Мария Завражина <takhosm@gmail.com>
2025-01-25 14:08:10 +0300

    change masks

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,16 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер карты в формате XXXX XX** **** XXXX"""
-    card_number_str = str(card_number)
-    return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number(7000792289606361))
+    print(get_mask_card_number("7000792289606361"))
 
 
-def get_mask_account(card_number: int) -> str:
+def get_mask_account(card_number: str) -> str:
     """Функция маскирует номер счета в формате **XXXX"""
-    card_number_str = str(card_number)
-    return f"**{card_number_str[-4:]}"
+    return f"**{card_number[-4:]}"
 
 
 if __name__ == "__main__":
-    print(get_mask_account(73654108430135874305))
+    print(get_mask_account("73654108430135874305"))
