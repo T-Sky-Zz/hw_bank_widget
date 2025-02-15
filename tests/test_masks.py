60480ab4ee1d48bb9d49767185ab93b9f39f8de2
Мария Завражина <takhosm@gmail.com>
2025-02-15 23:29:35 +0300

    test_masks fix

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,35 +3,35 @@ import pytest
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_card_number_correct():
-    """Тест проверяет, маскируется ли номер карты в формате XXXX XX** **** XXXX"""
-    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+@pytest.mark.parametrize("card_number, expected", [("7000792289606361", "7000 79** **** 6361"),
+                                                   ("1234567890123456", "1234 56** **** 3456")
+                                                   ])
+def test_get_mask_card_number_correct(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
 
 
-def test_get_mask_card_number_uncorrect():
-    """Тест проверяет, правильность введения данных"""
-    with pytest.raises(ValueError) as e_info:
-        get_mask_card_number("700079228960636113249876")
+def test_get_mask_card_number_incorrect():
+
+    with pytest.raises(ValueError):
+        get_mask_card_number("123456")
+    with pytest.raises(ValueError):
+        get_mask_card_number("1234567890123456789")
+    with pytest.raises(ValueError):
         get_mask_card_number("")
-        get_mask_card_number("700079")
-    with pytest.raises(TypeError) as e_info:
-        get_mask_card_number(7000792289606361)
-        get_mask_card_number({7000792289606361})
-        get_mask_card_number(["7000792289606361"])
 
 
-def test_get_mask_account_correct():
-    """Тест проверяет, маскируется ли номер счета в формате **XXXX"""
-    assert get_mask_account("73654108430135874305") == "**4305"
+@pytest.mark.parametrize("acc_number, expected", [("73654108430135874305", "**4305"),
+                                                  ("12345678901234567890", "**7890")
+                                                  ])
+def test_get_mask_account_correct(acc_number, expected):
+    assert get_mask_account(acc_number) == expected
 
 
-def test_get_mask_account_uncorrect():
-    """Тест проверяет, правильность введения данных"""
-    with pytest.raises(ValueError) as e_info:
-        get_mask_card_number("700079")
-        get_mask_card_number("700079228960636113249876")
-        get_mask_card_number("")
-    with pytest.raises(TypeError) as e_info:
-        get_mask_card_number(700079228960636113249876)
-        get_mask_card_number({700079228960636113249876})
-        get_mask_card_number(["700079228960636113249876"])
+def test_get_mask_account_incorrect():
+
+    with pytest.raises(ValueError):
+        get_mask_account("700079")
+    with pytest.raises(ValueError):
+        get_mask_account("73654108430135874305136")
+    with pytest.raises(ValueError):
+        get_mask_account("")
