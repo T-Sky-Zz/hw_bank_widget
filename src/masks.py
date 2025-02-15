e3c32604538777487d2be8bd971e2d0765ba781b
Мария Завражина <takhosm@gmail.com>
2025-02-15 19:17:42 +0300

    change masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,9 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер карты в формате XXXX XX** **** XXXX"""
+    if len(card_number) != 16:
+        raise ValueError("Введено неверное количество символов номера карты")
+    if type(card_number) is not str:
+        raise TypeError("Введен неверный тип данных")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
@@ -9,6 +13,10 @@ if __name__ == "__main__":
 
 def get_mask_account(card_number: str) -> str:
     """Функция маскирует номер счета в формате **XXXX"""
+    if len(card_number) != 20:
+        raise ValueError("Введено неверное количество символов номера счета")
+    if type(card_number) is not str:
+        raise TypeError("Введен неверный тип данных")
     return f"**{card_number[-4:]}"
 
 
