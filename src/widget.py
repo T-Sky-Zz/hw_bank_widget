f039afa6aae2a87003584cbb2280f00c6d909e08
Мария Завражина <takhosm@gmail.com>
2025-02-16 05:48:29 +0300

    widget fix

--- src/widget.py
+++ src/widget.py
@@ -3,30 +3,42 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(type_and_number: str) -> str:
     """Функция маскирует номер карты/счета"""
-    list_str = type_and_number.split(" ")  # — возвращает список слов в строке
-    number_account_card = list_str[-1]
-    if len(list_str[-1]) > 16:
-        list_str[-1] = get_mask_account(number_account_card)
+
+    if type(type_and_number) is str:
+        list_str = type_and_number.split(" ")
+        number_account_card = list_str[-1]
+
+        if list_str[-1].isdigit() and list_str[0].isalpha():
+
+            if len(list_str[-1]) == 16:
+                list_str[-1] = get_mask_card_number(number_account_card)
+            elif len(list_str[-1]) == 20:
+                list_str[-1] = get_mask_account(number_account_card)
+            else:
+                raise ValueError("Введены неверные данные номера счета/карты")
+                # print("Введены неверные данные номера счета/карты")
+
+            return " ".join(list_str)
+        else:
+            raise ValueError("Введены неверные данные счета/карты")
     else:
-        list_str[-1] = get_mask_card_number(number_account_card)
-    return " ".join(list_str)
+        raise TypeError("Неверный тип данных")
 
-
-if __name__ == "__main__":
-    print(mask_account_card("Maestro 1596837868705199"))
-    # print(mask_account_card("Счет 64686473678894779589"))
-    # print(mask_account_card("MasterCard 7158300734726758"))
-    # print(mask_account_card("Счет 35383033474447895560"))
-    # print(mask_account_card("Visa Classic 6831982476737658"))
-    # print(mask_account_card("Visa Platinum 8990922113665229"))
-    # print(mask_account_card("Visa Gold 5999414228426353"))
-    # print(mask_account_card("Счет 73654108430135874305"))
+# if __name__ == "__main__":
+#     print(mask_account_card("Maestro 1596837868705199"))
 
 
 def get_date(date: str) -> str:
     """Функция меняет формат даты на ДД.ММ.ГГГГ"""
-    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+    if type(date) is str:
+        list_date = date.split("-")  # — возвращает список слов в строке
 
+        if list_date[0].isdigit() and date[4] == "-" and date[7] == "-":
+            return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+        else:
+            raise ValueError("Введены неверные данные")
+    else:
+        raise TypeError
 
-if __name__ == "__main__":
-    print(get_date("2024-03-11T02:26:18.671407"))
+# if __name__ == "__main__":
+#     print(get_date("2024-03-11T02:26:18.671407"))
