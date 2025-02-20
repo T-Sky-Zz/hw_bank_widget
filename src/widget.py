354a65b79ef3db8a29d854b52e7211b8d11a522c
Мария Завражина <takhosm@gmail.com>
2025-02-20 04:32:02 +0300

    widget fix get_date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -19,26 +21,23 @@ def mask_account_card(type_and_number: str) -> str:
                 # print("Введены неверные данные номера счета/карты")
 
             return " ".join(list_str)
-        else:
-            raise ValueError("Введены неверные данные счета/карты")
-    else:
-        raise TypeError("Неверный тип данных")
+        raise ValueError("Введены неверные данные счета/карты")
+    raise TypeError("Неверный тип данных")
+
 
 # if __name__ == "__main__":
 #     print(mask_account_card("Maestro 1596837868705199"))
 
 
-def get_date(date: str) -> str:
+def get_date(input_date: str) -> str:
     """Функция меняет формат даты на ДД.ММ.ГГГГ"""
-    if type(date) is str:
-        list_date = date.split("-")  # — возвращает список слов в строке
+    if type(input_date) is str:
+        if datetime.strptime(input_date, "%Y-%m-%dT%H:%M:%S.%f"):
+            output_date = datetime.strptime(input_date, "%Y-%m-%dT%H:%M:%S.%f")
+            return output_date.strftime("%d.%m.%Y")
+        raise ValueError("Неверный формат даты")
+    raise TypeError
 
-        if list_date[0].isdigit() and date[4] == "-" and date[7] == "-":
-            return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
-        else:
-            raise ValueError("Введены неверные данные")
-    else:
-        raise TypeError
 
 # if __name__ == "__main__":
-#     print(get_date("2024-03-11T02:26:18.671407"))
+#     print(get_date("2024-03:26:18.671407"))
