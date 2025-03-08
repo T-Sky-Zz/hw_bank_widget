d635e578e55c50228c59386216e2faae68064d86
Мария Завражина <takhosm@gmail.com>
2025-03-08 03:16:24 +0300

    utils logging

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,18 @@
 import json
-# import os
+import logging
+import os
 from typing import Any
 
 from src.external_api import convert_currency
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "utils.log"), mode="w", encoding="utf-8"
+)
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def create_list_from_j_file(path: str) -> Any:
@@ -10,14 +20,19 @@ def create_list_from_j_file(path: str) -> Any:
     и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
     try:
+        logger.info(f"Поиск JSON-файла по заданному пути: {path}")
         with open(path, "r", encoding="utf-8") as json_data:
             data = json.load(json_data)
+            logger.info(f"Функция возвращает список словарей с данными о финансовых транзакциях \n{data}")
             return data
     except json.JSONDecodeError:
+        logger.error("Ошибка: Файл пуст")
         return []
     except TypeError:
+        logger.error("Ошибка: Файл содержит не список")
         return []
     except FileNotFoundError:
+        logger.error("Ошибка: Файл не найден")
         return []
 
 
@@ -29,15 +44,20 @@ def create_list_from_j_file(path: str) -> Any:
 def get_conversion_amount(transaction: dict) -> float:
     """Функция, принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
     Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют
-    и конвертации суммы операции в рубли. """
+    и конвертации суммы операции в рубли."""
+    logger.info("Определение валюты транзакции")
     amount = transaction.get("operationAmount", {}).get("amount")
     currency_code = transaction.get("operationAmount", {}).get("currency", {}).get("code")
+    logger.info(f"Валюта: {currency_code}")
     if currency_code == "RUB":
+        logger.info(f"Сумма транзакции: {amount} руб.")
         return float(amount)
     elif currency_code in ["USD", "EUR"]:
         result = convert_currency(currency_code, "RUB", amount)
+        logger.info(f"Сумма транзакции конвертированная: {amount} руб.")
         return round(result, 2)
     else:
+        logger.error("Ошибка: иная валюта транзакции")
         return 0.00
 
 
